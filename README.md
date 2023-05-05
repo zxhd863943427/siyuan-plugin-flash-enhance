29f1131a9171885aca4a2c2263875519d2b28b60
Vanessa <lly219@gmail.com>
2023-05-05 12:51:15 +0800

    :art: zip dist

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # plugin-sample
 SiYuan plugin sample
 
+## Release
+
+- `pnpm run build` to general dist and zip.
+- Create new GitHub release using your new version number as the "Tag version". See here for an example: https://github.com/siyuan-note/plugin-sample/releases
+- Upload the files `*.zip` as binary attachments.
+- Publish the release.
+
 ## How to use
 
 - `pnpm i` to install dependencies
@@ -8,4 +15,5 @@ SiYuan plugin sample
 
 ## Manually installing the plugin
 
+- `pnpm run build` to general dist.
 - Copy all files under `dist` to `Workspace/data/plugins/your-plugin-name/`.
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,39 @@ const {EsbuildPlugin} = require("esbuild-loader");
 const webpack = require("webpack");
 const fs = require("fs");
 const CopyPlugin = require("copy-webpack-plugin");
+const ZipPlugin = require('zip-webpack-plugin');
+const pluginJSON = require("./plugin.json");
 
 module.exports = (env, argv) => {
+    const plugins = [
+        new MiniCssExtractPlugin({
+            filename: "dist/index.css",
+        })
+    ]
+    if (argv.mode === "production") {
+        plugins.push(new webpack.BannerPlugin({
+            banner: () => {
+                return fs.readFileSync("LICENSE").toString();
+            },
+        }))
+        plugins.push(new CopyPlugin({
+            patterns: [
+                {from: "preview.png", to: "./dist/preview.png"},
+                {from: "icon.png", to: "./dist/icon.png"},
+                {from: "README.md", to: "./dist/README.md"},
+                {from: "plugin.json", to: "./dist/plugin.json"},
+            ],
+        }))
+        plugins.push(new ZipPlugin({
+            filename: `${pluginJSON.name}-${pluginJSON.version}.zip`,
+            algorithm: "gzip",
+            include: [/dist/],
+            pathPrefix: `${pluginJSON.name}`,
+            pathMapper: (assetPath) => {
+                return assetPath.replace("dist/", "")
+            },
+        }))
+    }
     return {
         mode: argv.mode || "development",
         watch: argv.mode !== "production",
@@ -13,13 +44,13 @@ module.exports = (env, argv) => {
         output: {
             filename: "[name].js",
             libraryTarget: "commonjs",
-            path: path.resolve(__dirname, "./dist/"),
+            path: path.resolve(__dirname),
         },
         externals: {
             siyuan: "siyuan",
         },
         entry: {
-            "index": "./src/index.ts",
+            "dist/index": "./src/index.ts",
         },
         optimization: {
             minimize: true,
@@ -59,22 +90,6 @@ module.exports = (env, argv) => {
                 }
             ],
         },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: "index.css",
-            }),
-            new webpack.BannerPlugin({
-                banner: () => {
-                    return fs.readFileSync("LICENSE").toString();
-                },
-            }),
-            new CopyPlugin({
-                patterns: [
-                    { from: "preview.png", to: "preview.png" },
-                    { from: "README.md", to: "README.md" },
-                    { from: "plugin.json", to: "plugin.json" },
-                ],
-            }),
-        ],
+        plugins,
     };
 };
