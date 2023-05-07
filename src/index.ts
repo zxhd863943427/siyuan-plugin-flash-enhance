c8e0d035bec4a5e40292c0edf4e07c3b7633b301
Vanessa <lly219@gmail.com>
2023-05-07 17:44:26 +0800

    :art: :art: https://github.com/siyuan-note/siyuan/issues/5066

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
-import {Plugin} from "siyuan";
+import {Plugin, Message, Confirm} from "siyuan";
 import "./index.scss";
 
 export default class MyPlugin extends Plugin {
     onload() {
-        console.log(this.i18n.helloPlugin);
         this.eventBus.on("ws-main", ({detail}: any) => {
             console.log(detail);
         });
+        Confirm("Confirm", "Is this a confirm?", () => {
+            console.log("confirm");
+        }, () => {
+            console.log("cancel");
+        })
+
+        Message(this.i18n.helloPlugin);
     }
 
     onunload() {
         console.log(this.i18n.byePlugin);
+        this.eventBus.off("ws-main", ({detail}: any) => {
+            console.log(detail);
+        });
     }
 }
