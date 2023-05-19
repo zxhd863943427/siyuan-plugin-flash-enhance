496c67b8593587d26ec71d36edf2f316c520d864
zxhd <zxhd863943427@outlook.com>
2023-05-19 17:58:29 +0800

    update card.d.ts

--- src/utils/card.d.ts
+++ src/utils/card.d.ts
@@ -1,4 +1,4 @@
 import * as card from "./card.js"
-declare function addCards():any
+declare function addCards(useBulitIn:boolean):any
 declare function removeCards():any
 declare function openDynamiMarkCard(openDynamiMark:boolean, useBulitIn:boolean):any
