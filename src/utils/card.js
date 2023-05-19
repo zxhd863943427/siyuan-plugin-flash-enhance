44b6b05b9191611cd1163693dc97c753afef5296
zxhd <zxhd863943427@outlook.com>
2023-05-19 20:07:37 +0800

    重构并清理未使用代码

--- src/utils/card.js
+++ src/utils/card.js
@@ -34,17 +34,29 @@ function getBlock(node){
 }
 
 
-// 添加多个卡片
-export async function addCards(useBulitIn = true) {
-    //获取全部deck
+export async function getCurrentDeck(){
     let res = await request("/api/riff/getRiffDecks", {});
 
     //获取当前打开文档的路径
     let focusItemPath = await getHpath()
     if (focusItemPath === null) {
         showMessage(`未能当前文档读取路径`);
-        return;
+        return [];
     }
+    let customDeckIdArr = [];
+    //获取deck.id, 其deck.name 包含在 focusItemPath 中
+    Array.from(res.data).forEach((item) => {
+        let name = item.name;
+        if (focusItemPath.search(name) != -1) {
+            customDeckIdArr.push(item.id);
+        }
+    })
+    return customDeckIdArr
+}
+
+
+// 添加多个卡片
+export async function addCards(useBulitIn = true) {
 
 
     let customDeckIdArr = [];
@@ -53,16 +65,12 @@ export async function addCards(useBulitIn = true) {
         customDeckIdArr.push('20230218211946-2kw8jgx');
     }
     else {
-
-        //获取deck.id, 其deck.name 包含在 focusItemPath 中
-        Array.from(res.data).forEach((item) => {
-            let name = item.name;
-            if (focusItemPath.search(name) != -1) {
-                customDeckIdArr.push(item.id);
-            }
-        })
+        customDeckIdArr = await getCurrentDeck()
     }
 
+    if (customDeckIdArr === []){
+        return;
+    }
 
 
     if (customDeckIdArr.length === 0) {
@@ -150,105 +158,4 @@ export async function removeCards() {
         showMessage(`All卡包的移除卡片数：${arr.length}`);
     }
 
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////
-
-function fliterDyMarkCard(node) {
-
-    let nodeParent = node.parentElement.parentElement
-    //判断是否已经制卡
-    if (nodeParent.getAttribute("custom-riff-decks")) {
-        return {
-            status: "No a todo card",
-            data: nodeParent.getAttribute("data-node-id")
-        }
-    }
-    return {
-        status: "ok",
-        data: nodeParent.getAttribute("data-node-id")
-    }
-}
-
-async function dynamiMarkCard(useBulitIn) {
-
-    //获取全部deck
-    let res = await request("/api/riff/getRiffDecks", {});
-
-    //获取当前打开文档的路径
-    let focusItemPath = await getHpath()
-    if (focusItemPath === null) {
-        showMessage(`未能当前文档读取路径`);
-        return;
-    }
-
-    let customDeckIdArr = [];
-    // 导入默认卡包
-    if (useBulitIn === true) {
-        customDeckIdArr.push('20230218211946-2kw8jgx');
-    }
-    else {
-
-        //获取deck.id, 其deck.name 包含在 focusItemPath 中
-        Array.from(res.data).forEach((item) => {
-            let name = item.name;
-            if (focusItemPath.search(name) != -1) {
-                customDeckIdArr.push(item.id);
-            }
-        })
-    }
-
-    if (customDeckIdArr.length === 0) {
-        showMessage(`未能获取到：${focusItemPath} 的卡包`);
-        return;
-    }
-    //获取选中位置的情况
-    let selectParent = document.getSelection().anchorNode.parentElement
-
-    //如果为selectParent.tagName === "SPAN"，说明为标记，进入制卡流程
-    if (selectParent.tagName === "SPAN") {
-        setTimeout(async ()=>{await dyAddCard();}, 500)
-    }
-    else{
-        //否则说明为取消标记，需要判断是否取消了所有标记
-        setTimeout(async ()=>{await removeCard();}, 500)
-    }
-    async function dyAddCard() {
-        let MarkCardList = iterArr([selectParent], fliterDyMarkCard);
-
-        for (let deckIndex in customDeckIdArr) {
-            let body = {
-                deckID: customDeckIdArr[deckIndex],
-                blockIDs: MarkCardList,
-            };
-
-            let res = await request("/api/riff/addRiffCards", body);
-        }
-        return;
-    }
-
-    async function removeCard() {
-        // console.log("取消标记")
-        let markNode = selectParent.querySelectorAll("span[data-type='mark']");
-        // console.log("markNode ",markNode)
-        if (markNode.length <= 0) {
-            let MarkCardList = [selectParent.parentElement.getAttribute("data-node-id")];
-            // console.log("MarkCardList ",MarkCardList)
-            let body = {
-                deckID: '',
-                blockIDs: MarkCardList,
-            };
-            // console.log("body ",body)
-            let res = await request("/api/riff/removeRiffCards", body);
-            // console.log("res ",res.code)
-        }
-    }
-}
-
-export async function openDynamiMarkCard(openDynamiMark, useBulitIn){
-    if (!openDynamiMark){
-        return;
-    }
-    setTimeout(()=>{dynamiMarkCard(useBulitIn)}, 100)
-    // dynamiMarkCard(useBulitIn)
 }
