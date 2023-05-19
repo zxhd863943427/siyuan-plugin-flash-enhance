7a1eee117161ba41c55adc9a6b57fb2060557ed6
zxhd <zxhd863943427@outlook.com>
2023-05-19 19:57:38 +0800

    完成动态制卡

--- src/api/dyCard.ts
+++ src/api/dyCard.ts
@@ -1,21 +1,54 @@
 import { isDoMark, isDelete, isUndo, isCarded } from "../lib/status"
 import { foreach } from "../lib/utils"
+import { settingList } from "../utils/config"
+import { fetchPost, showMessage } from "siyuan"
+
+const builtInDeck = '20230218211946-2kw8jgx'
+
 export function dyMakeCard(detail: any, plugin: any) {
     console.log("编辑操作")
     console.log(detail)
     foreach(detail.data, (data: any) => {
         // console.log("是否为撤回操作:",isUndo(data))
-        foreach(data.doOperations,(item:any)=>{
+        foreach(data.doOperations, (item: any) => {
             let type = item.action
             let marked = isDoMark(item)
             let carded = isCarded(item)
-            let needMakeCard = ((type ==="insert" || type === "update") && marked && !carded)
+            let open = settingList.getSetting()["沉浸式制卡"]
+            let needMakeCard = ((type === "insert" || type === "update") && marked && !carded)
             let needDelCard = ((type === "update" && !marked && carded) || (type === "delete"))
-            console.log(item.id,"操作类型:",item.action,
-                "\n是否已经制卡:\t",carded,
-                "\n是否需要制卡:\t",needMakeCard,
-                "\n是否需要取消制卡:\t",needDelCard,
-                )
+            console.log(item.id, "操作类型:", item.action,
+                "\n是否已经制卡:\t", carded,
+                "\n是否需要制卡:\t", needMakeCard,
+                "\n是否需要取消制卡:\t", needDelCard,
+                "\n是否打开动态制卡:\t", open
+            )
+            if (needMakeCard){
+                addCard(item.id)
+            }
+            if (needDelCard){
+                removeCard(item.id)
+            }
         })
     })
+}
+
+async function addCard(id: string){
+    let body = {
+        deckID: builtInDeck,
+        blockIDs: [id],
+    };
+    fetchPost("/api/riff/addRiffCards", body, (res)=>{
+        showMessage(`${res.data.name}卡包的总卡片数：${res.data.size}`);
+    });
+}
+
+async function removeCard(id: string){
+    let body = {
+        deckID: builtInDeck,
+        blockIDs: [id],
+    };
+    fetchPost("/api/riff/removeRiffCards", body, (res)=>{
+        showMessage(`${res.data.name}卡包的剩余卡片数：${res.data.size}`);
+    });
 }
