28c90104d4b6ab24834efef0d8983aee77dea491
zxhd <zxhd863943427@outlook.com>
2023-06-30 12:52:57 +0800

    增加列表动态制卡

--- src/api/dyCard.ts
+++ src/api/dyCard.ts
@@ -1,4 +1,4 @@
-import { isDoMark, isDelete, isUndo, isCarded } from "../lib/status"
+import { isDoMark, isDelete, isUndo, isCarded, isDoListCard, isHasListCardMark, isParentCarded, getParentElement, isParentIsList } from "../lib/status"
 import { foreach } from "../lib/utils"
 import { settingList } from "../utils/config"
 import { fetchPost, showMessage } from "siyuan"
@@ -18,6 +18,8 @@ export function dyMakeCard(detail: any, plugin: any) {
         foreach(data.doOperations, (item: any) => {
             console.log("\n是否打开动态制卡:\t", open)
             makeMarkCard(item)
+            makeListCard(item)
+            autoDeleteCard(item)
         })
     })
 }
@@ -27,7 +29,7 @@ function makeMarkCard(item: any) {
     let marked = isDoMark(item)
     let carded = isCarded(item)
     let needMakeCard = ((type === "insert" || type === "update") && marked && !carded)
-    let needDelCard = ((type === "update" && !marked && carded) || (type === "delete"))
+    let needDelCard = ((type === "update" && !marked && carded))
     console.log(item.id, "操作类型:", item.action,
         "\n是否已经制卡:\t", carded,
         "\n是否需要制卡:\t", needMakeCard,
@@ -36,6 +38,44 @@ function makeMarkCard(item: any) {
     if (needMakeCard) {
         addCard(item.id)
     }
+    if (needDelCard) {
+        removeCard(item.id)
+    }
+}
+
+function makeListCard(item: any) {
+    let type = item.action
+    let ListCarded = isDoListCard(item)
+    let ListCardMarked = isHasListCardMark(item)
+    let parentIsList = isParentIsList(item)
+    let carded = isParentCarded(item)
+    let needMakeCard = ((type === "insert" || type === "update") && ListCarded && !carded)
+    let needDelCard = (type === "update" && parentIsList && carded && !ListCarded && !ListCardMarked)
+    console.log(item.id, "操作类型:", item.action,
+        "\n是否父元素为列表:\t", parentIsList,
+        "\n是否使用列表标记:\t", ListCarded,
+        "\n是否存在原始列表标记:\t", ListCardMarked,
+        "\n是否已经制卡:\t", carded,
+        "\n是否需要制卡:\t", needMakeCard,
+        "\n是否需要取消制卡:\t", needDelCard
+    )
+    if (needMakeCard) {
+        let listElement = getParentElement(item)
+        if (listElement === null) return
+        let id = listElement.getAttribute("data-node-id")
+        addCard(id)
+    }
+    if (needDelCard) {
+        let listElement = getParentElement(item)
+        if (listElement === null) return
+        let id = listElement.getAttribute("data-node-id")
+        removeCard(id)
+    }
+}
+
+function autoDeleteCard(item:any){
+    let type = item.action
+    let needDelCard = ((type === "delete"))
     if (needDelCard) {
         removeCard(item.id)
     }
--- src/lib/status.ts
+++ src/lib/status.ts
@@ -1,4 +1,5 @@
 import { exist } from "./utils"
+import { getCurrentPage } from "../utils/utils"
 
 export function isDoMark(item: any) {
     if (item.data === null) {
@@ -10,6 +11,72 @@ export function isDoMark(item: any) {
     // console.log(item.data)
     // console.log(query)
     return query.length != 0
+}
+
+export function isDoListCard(item:any):boolean{
+
+    let DoListCard = false
+    let element = document.createElement("div")
+    element.innerHTML = item.data
+
+    let sourceElement = getParentElement(item)
+    if (sourceElement === null) //无法找到父节点，证明不是列表，返回false
+        return false
+    let parentIsList = isParentIsList(item)
+    if (!parentIsList)
+        return false
+    let query = element.querySelectorAll(`[data-type~="strong"]`)
+    for (let node of query){
+        let innerText = (node as HTMLElement).innerText
+        if (innerText === "?" || innerText === "？")
+            DoListCard = true
+    }
+    return DoListCard
+}
+
+export function isHasListCardMark(item:any):boolean{
+    if (item.data === null) {
+        return false
+    }
+    let re = /(>>|(》》)|(\|\|)|(\?)|(？))\s?\u200b?$/;
+    let deleteListCarded = false
+    let element = document.createElement("div")
+    element.innerHTML = item.data
+    if (re.test(element.innerText))
+        deleteListCarded = true
+    return deleteListCarded
+}
+
+export function isParentIsList(item:any):boolean{
+
+    let sourceElement = getParentElement(item)
+    let dataType = sourceElement ? sourceElement.getAttribute("data-type") : "none"
+    if (sourceElement === null || dataType != "NodeListItem") //无法找到nodelist的父节点，证明不是列表，返回false
+        return false
+    else{
+        return true
+    }
+}
+
+export function isParentCarded(item:any):boolean{
+
+    let sourceElement = getParentElement(item)
+    if (sourceElement === null) //无法找到nodelist的父节点，证明不是列表，返回false
+        return false
+    let query = sourceElement.getAttribute("custom-riff-decks")
+    return query != null
+}
+
+export function getParentElement(item:any):Element|null{
+    if (item.data === null || (typeof(item.data)) != "string") {
+        return null
+    }
+    let element = document.createElement("div")
+    element.innerHTML = item.data
+    let dataId = (element.childNodes[0] as HTMLElement).getAttribute("data-node-id")
+    let currentPage = getCurrentPage()
+    let sourceElement = currentPage.querySelector(`[data-type]:has(>[data-node-id="${dataId}"])`)
+    return sourceElement
 }
 
 export function isCarded(item: any) {
